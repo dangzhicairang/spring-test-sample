fe6bd924cdbe35f03fb7b7df9b24ba448afe4a17
dzcr <1137729123@qq.com>
2022-05-06 17:45:54 +0800

    init

---
+++ src/test/java/com/xsn/springtestdemo/SpringTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsn.springtestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
